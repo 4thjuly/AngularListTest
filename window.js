d285f9ff6eb67d142280eafc4a35fdd7ec656afd
4thJuly <4thJuly>
2013-08-19 14:55:41 -0700

    Better strings

--- window.js
+++ window.js
@@ -5,15 +5,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function ListController($scope) {
     $scope.list = [
-        {name:'Item One'},
-        {name:'Item Two'},
-        {name:'Item Three'}
+        {friendlyName:'HOMESERVER: 1 : Windows Media Connect', make:'Microsoft', model:'Windows Media Connect'},
+        {friendlyName:'HOMESERVER', make:'Hewlett Packard', model:'Windows Home Server'},
+        {friendlyName:'Whole House', make:'Logitech', model:'Squeezebox Touch'},
+        {friendlyName:'Living Room Camera (192.168.1.164)', make:'TOSHIBA', model:'Wireless Network Camera'},
+        {friendlyName:'Logitech Media Server [Homeserver]', make:'Logitech', model:'Logitech Media Server 7.7.2 r33893'}
     ];
  
   $scope.refresh = function() {
-      // TODO Refresh something
       console.log("Refresh");
-      $scope.list.push({name:'Another'});
+      $scope.list.push({friendlyName:'Friend Name', make:'Make', model:'Model'});
   };
  
 }
